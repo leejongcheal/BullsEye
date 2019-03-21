6f7d2b04d1232675972b3a5933669a0d37a7a29a
Myoung-Wan Koo <mwkoo9@gmail.com>
2019-03-21 14:12:17 +0900

    Slider Move 출력 완성

--- BullsEye/ViewController.swift
+++ BullsEye/ViewController.swift
@@ -9,15 +9,16 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    var currentValue: Int = 50
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
 
     @IBAction func showAlert(){
+        let message = "The value of the slider is: \(currentValue)"
         let alert = UIAlertController(title: "Hello, World",
-                                      message: "This is my first app!",
+                                      message: message,
                                       preferredStyle: .alert)
         
         let action = UIAlertAction(title: "Awesome", style: .default,
@@ -28,5 +29,10 @@ class ViewController: UIViewController {
         present(alert, animated: true, completion: nil)
 
     }
+    @IBAction func sliderMoved(_ slider: UISlider) {
+        currentValue = lroundf(slider.value)
+        print(" The value of the slider is now:\(currentValue)")
+    }
+
 }
 
